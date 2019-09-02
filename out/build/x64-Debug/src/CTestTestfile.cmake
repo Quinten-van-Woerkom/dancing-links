6567ffc862f9bd3f1958b6081e90aa8cf9e40dac
Quinten van Woerkom <quintenvanwoerkom@gmail.com>
2019-09-02 22:58:22 +0200

    Removed out from git

--- out/build/x64-Debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Coding/dancing-links/src
-# Build directory: D:/Coding/dancing-links/out/build/x64-Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Coding/dancing-links/tests
-# Build directory: D:/Coding/dancing-links/out/build/x64-Debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DancingLinksTest "dancing_links_test")
-set_tests_properties(DancingLinksTest PROPERTIES  _BACKTRACE_TRIPLES "D:/Coding/dancing-links/tests/CMakeLists.txt;5;add_test;D:/Coding/dancing-links/tests/CMakeLists.txt;0;")
